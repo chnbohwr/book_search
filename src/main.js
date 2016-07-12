d231560eb043da0aedcce826a10c1acfb5414c3f
Hyman_Chen <hyman_chen@wistron.com>
2016-07-12 13:06:45 +0800

    add simple store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+
+// import router
 import VueRouter from 'vue-router'
+// import router setting
 import RouterSetting from './router/config.js'
+// import simple store
 import VueSimpleStore from './libs/vue-simple-store.min.js';
+
+
 
 Vue.use(VueRouter);
 
